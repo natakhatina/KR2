0ac3f2ec8f3d8a30f1a7af6cf9d3a2ccc5a8e7c8
Наталья <natakhatina@gmail.com>
2018-10-24 23:01:14 +0300

    4

--- KR2_script.py
+++ KR2_script.py
@@ -4,15 +4,24 @@
 
 #2
 import math
+#import matplotlib.pyplot as plt
 
-o=1 # сигма
-u=0 # мат. ожидание
+# o=1 # сигма
+# u=0 # мат. ожидание
+#
+# def res(x):
+#     res=(1/(o*math.sqrt(2*math.pi)))*math.exp(-(x-u)**2/(2*o**2))
+#     return res
+#
+# NormRasp={x:res(x) for x in range(-100,100)}
+#
+# for i in range(-10,11):
+#     print (i,' : ',NormRasp[i])
 
-def res(x):
-    res=(1/(o*math.sqrt(2*math.pi)))*math.exp(-(x-u)**2/(2*o**2))
-    return res
+#4
+def foo(n):
+    for i in range(n):
+        yield (-1)**(i+1)
 
-NormRasp={x:res(x) for x in range(-100,100)}
-
-for i in range(-10,11):
-    print (i,' : ',NormRasp[i])
+L=[i for i in foo(30)]
+print(L)
